fe7ebd16deea799e4197d0f39d11398ac8eb6c21
Letícia Faria <leticianfaria@hotmail.com>
2018-09-05 21:46:09 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-
 function cardValidator(num) {
-  try {
-    if (num === "" || num === undefined) {
-      throw new Error("Está faltando parâmetro!");
-    } else if (typeof num !== "number") {
-      throw new Error("Digite apenas números!");
-    } else if (Number.isInteger(num) && num.toString().length === 1) {
-      throw new Error("O número informado contém apenas um digito!");
-    } else {
-      let reverseNumber = String(num).split("").reverse();
-      let evenPosition = [];
-      let oddPosition = [];
-      for (let i in reverseNumber) {
-        if (i % 2 !== 0) {
-          reverseNumber[i] * 2 > 9 ? evenPosition.push((reverseNumber[i] * 2) - 9) : evenPosition.push(reverseNumber[i] * 2);
-        } else {
-          oddPosition.push(parseInt(reverseNumber[i]));
-        }
+  if (num === "" || num === undefined) {
+    throw new Error("Está faltando parâmetro!");
+  } else if (typeof num !== "number") {
+    throw new Error("Digite apenas números!");
+  } else if (Number.isInteger(num) && num.toString().length === 1) {
+    throw new Error("O número informado contém apenas um digito!");
+  } else {
+    const reverse = String(num).split("").reverse();
+    let even = [];
+    let odd = [];
+    reverse.forEach((el, i) => {
+      if (i % 2 !== 0) {
+        el * 2 > 9 ? even.push((el * 2) - 9) : even.push(el * 2);
+      } else {
+        odd.push(parseInt(el));
       }
-      let concat = evenPosition.concat(oddPosition);
-      let sum = concat.reduce((prev, elem) => prev + elem);
-      return (sum % 10 === 0) ? true : false;
-    }
-  } catch (e) {
-    return e.message;
+    });
+    const concat = even.concat(odd);
+    const sum = concat.reduce((prev, elem) => prev + elem);
+    return sum % 10 === 0;
   }
 }
 module.exports.cardValidator = cardValidator;
-// module.exports = cardValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,28 +7,28 @@ describe("index", function () {
   describe("#cardValidator()", function () {
     describe("Quando nao houver parâmetro", function () {
       it("deve lancar um erro", function () {
-        expect(index.cardValidator()).to.equal("Está faltando parâmetro!");
-        expect(index.cardValidator("")).to.equal("Está faltando parâmetro!");
+        expect(() => index.cardValidator()).to.throws("Está faltando parâmetro!");
+        expect(() => index.cardValidator("")).to.throws("Está faltando parâmetro!");
       });
     });  
     describe("Quando o número for uma string", function () {
       it("deve lançar um erro", function () {
-        expect(index.cardValidator("abc")).to.equal("Digite apenas números!");
+        expect(() => index.cardValidator("abc")).to.throws("Digite apenas números!");
       });
     });  
     describe("Quando o número for um inteiro e houver um digito", function () {
       it("deve lançar um erro", function () {
-        expect(index.cardValidator(3)).to.equal("O número informado contém apenas um digito!");
+        expect(() => index.cardValidator(3)).to.throws("O número informado contém apenas um digito!");
       });
     });  
     describe("Quando o número for um inteiro e for um cartão válido", function () {
       it("deve retornar true", function () {
-        expect(index.cardValidator(36490102462661)).to.equal(true);
+        expect(index.cardValidator(36490102462661)).to.be.true;
       });
     }); 
     describe("Se o cartão for inválido", function () {
       it("deve retornar false", function () {
-        expect(index.cardValidator(12345)).to.equal(false);
+        expect(index.cardValidator(12345)).to.be.false;
       });
     }); 
   });
